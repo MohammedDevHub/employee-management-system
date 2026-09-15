e1841451ccfc8a5d60bf330a614e62a8c807bc73
Mohammed Emad <me6408852@gmail.com>
2026-09-15 16:18:18 +0300

    Update Next.js config for output and basePath

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  output: "export",
+
+  basePath: "/employee-management-system",
+
+  images: {
+    unoptimized: true,
+  },
 };
 
 module.exports = nextConfig;
